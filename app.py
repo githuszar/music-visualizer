10d265169574de74c0d10e4e3d7e878a53cc0bc2
githuszar <githuszar@gmail.com>
2025-03-13 22:45:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,15 @@ import os
 import requests
 import pandas as pd
 import streamlit as st
-from spotipy.oauth2 import SpotifyOAuth
-import spotipy
+
+# Verifica se a biblioteca Spotipy está instalada, instala se necessário
+try:
+    from spotipy.oauth2 import SpotifyOAuth
+    import spotipy
+except ModuleNotFoundError:
+    os.system('pip install spotipy')
+    from spotipy.oauth2 import SpotifyOAuth
+    import spotipy
 
 # Configuração inicial utilizando Streamlit Secrets
 CLIENT_ID = st.secrets["CLIENT_ID"]
